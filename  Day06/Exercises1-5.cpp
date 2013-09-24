642bd6be4e35319e5b20b433a40f738209ee4f56
Kolxein <fomenti@rambler.ru>
2013-09-24 13:06:54 +0300

    lesen 6

---
+++  Day06/Exercises1-5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+	public:
+		Employee(int a, int b, int c);
+		void Age(int age){itsage = age;}
+		int GetAge()const {return itsage;}
+		void YearsOfService(int yearsServis){itsyearsOfService = yearsServis;}
+		int GetYearsOfService()const {return itsyearsOfService;}
+		void Salary(int salary){itssalary = salary;}
+		int GetSalary()const {return itssalary;}
+		int Earnings()const {return 12 * itsyearsOfService * itssalary;}
+	private:		
+		int itsage;
+		int itsyearsOfService;
+		int itssalary;
+};
+Employee::Employee(int a, int b, int c)
+	:itsage(a)
+	,itsyearsOfService(b)
+	,itssalary(c)
+{
+	cin >> a;
+	itsage = a;
+	cin >> b;
+	itsyearsOfService = b;
+	cin >> c;
+	itssalary = c;
+}
+int main()
+{
+	Employee Fil(0, 0, 0);
+	cout << Fil.GetAge() << "\n";
+	cout << Fil.GetYearsOfService() << "\n";
+	cout << Fil.GetSalary() << "\n";
+	cout << Fil.Earnings() << "\n\n";
+	Employee Bob(0, 0, 0);
+	cout << Bob.GetAge() << "\n";
+	cout << Bob.GetYearsOfService() << "\n";
+	cout << Bob.GetSalary() << "\n";
+	cout << Bob.Earnings();
+	return 0;
+}
+
