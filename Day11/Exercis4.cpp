fc8b02092acba455f3e2fb15142c178068ce4fa7
Kolxein <fomenti@rambler.ru>
2013-10-17 16:14:54 +0300

    day11

--- Day11/Exercis4.cpp
+++ Day11/Exercis4.cpp
@@ -3,33 +3,19 @@
 using namespace std;
 class Shape
 {
-	public:
-		Shape():itsPoint(int (1)){};
-		Shape(int);
+	protected:
+		Shape(){};
 		Shape (const Shape&);
 		virtual ~Shape(){};
 		virtual Shape* Clone(){return new Shape(*this);}
-		int GetPoint() const {return itsPoint;}
-		void SetPoint(int point) {itsPoint = point;}
-	protected:
-		int itsPoint;
 };
 
-Shape::Shape(int r)
-	:itsPoint(r)
-{
-};
-Shape::Shape(const Shape& rhs):itsPoint(rhs.GetPoint())
-{
-};
+Shape::Shape(const Shape& rhs)
+{};
 
 class Rectangle : public Shape
 {
 	public:
-		Rectangle():Shape()
-			,itsLength(int (0))
-			,itsWidth(int (0)){};
-		Rectangle(int);
 		Rectangle(int, int);
 		Rectangle(const Rectangle&);
 		virtual ~Rectangle(){};
@@ -43,53 +29,43 @@ class Rectangle : public Shape
 		int itsWidth;
 };
 
-Rectangle::Rectangle(int length)
-	:Shape()
-	,itsLength(length)
-	,itsWidth(int (0))
-{
-};
-Rectangle::Rectangle(int length,int width)
-	:itsLength(length)
-	,itsWidth(width)
-{
-};
-Rectangle::Rectangle(const Rectangle& rhs):Shape(rhs)
-{
-};
+Rectangle::Rectangle(int length, int width)
+	: itsLength(length)
+	, itsWidth(width)
+{};
+
+Rectangle::Rectangle(const Rectangle& rhs)
+	: Shape(rhs)
+	, itsLength(rhs.GetLength())
+	, itsWidth(rhs.GetWidth())
+{};
 
 class Square : public Rectangle
 {
 	public:
-		Square();
 		Square(int);
 		Square(const Square&);
 		virtual ~Square(){};
 		virtual Square* Clone(){return new Square(*this);}
 };
-Square::Square(const Square& rhs):Rectangle(rhs){};
+
+Square::Square(const Square& rhs)
+	: Rectangle(rhs.itsLength, rhs.itsLength)
+{};
 
 Square::Square(int length)
-	//:Rectangle(l)
-{
-	SetLength(length);
-};
+	: Rectangle(length, length)
+{};
 
 int main()
 {
-	Shape umolchania(7);
-	Rectangle smol;
 	Rectangle mol(2,8);
 	Square big(12);
-	smol.SetPoint(5);
-	cout << "Center Point umolchania = " << umolchania.GetPoint() << "\n";
-	cout << "Center Point smol = " << smol.GetPoint() << "\n";
-	cout << "Center Point big = " << big.GetPoint() << "\n";
-	cout << "Length big = " << big.GetLength() << "\n";
+	cout << "Length Square big = " << big.GetLength() << "\n";
+	cout << "Width Square big = " << big.GetWidth() << "\n";
 	cout << "\n";
-	cout << "Center Point mol = " << mol.GetPoint() << "\n";
-	cout << "Length mol = " << mol.GetLength() << "\n";
-	cout << "Width mol = " << mol.GetWidth() << "\n";
+	cout << "Length Rectangle mol = " << mol.GetLength() << "\n";
+	cout << "Width Rectangle mol = " << mol.GetWidth() << "\n";
 	return 0;
 };
 
